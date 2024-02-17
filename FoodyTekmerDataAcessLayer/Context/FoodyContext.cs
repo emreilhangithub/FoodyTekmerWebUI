a9690439c6ca08bd5316b0b5b43d2b4e13d15d74
musta <musta@DESKTOP-I1U4DR9>
2024-02-17 12:26:32 +0300

    Add project files.

---
+++ FoodyTekmerDataAcessLayer/Context/FoodyContext.cs
@@ -0,0 +1,22 @@
+﻿using FoodyTekmerEntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodyTekmerDataAccessLayer.Context
+{
+    public class FoodyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-I1U4DR9;initial catalog=FoodyTekmerEntityDb;Integrated Security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
