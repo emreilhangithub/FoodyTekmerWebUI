5b732bbceb7c9336a9ea904b34d81626ee308892
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2024-02-17 15:35:04 +0300

    crud işlemleri tamamlandı ve tema indirildi

--- FoodyTekmerWebUI/Controllers/CategoryController.cs
+++ FoodyTekmerWebUI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using FoodyTekmerDataAccessLayer.Context;
+using FoodyTekmerEntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodyTekmerWebUI.Controllers
@@ -8,8 +9,43 @@ namespace FoodyTekmerWebUI.Controllers
         FoodyContext context = new FoodyContext();
         public IActionResult Index()
         {
-            var values = context.Categories;
+            var values = context.Categories.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
             return View();
         }
+        [HttpPost]
+        public IActionResult CreateCategory(Category p)
+        {
+            context.Categories.Add(p);
+            context.SaveChanges();//Kayıt işlemini tamamladık.
+            return RedirectToAction("Index"); //Anasayfaya yönlendirme işlemi yapar.
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            context.Categories.Remove(value);
+            context.SaveChanges();//Kayıt işlemini tamamladık.
+            return RedirectToAction("Index"); //Anasayfaya yönlendirme işlemi yapar.
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {            
+            context.Categories.Update(category);
+            context.SaveChanges();//Kayıt işlemini tamamladık.
+            return RedirectToAction("Index"); //Anasayfaya yönlendirme işlemi yapar.
+        }
+
     }
 }
