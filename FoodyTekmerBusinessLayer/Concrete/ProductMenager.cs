23aba864f119b07669e055a82456a14b7794a364
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-02-18 12:50:56 +0300

    Menager sınıfıyla interfacelere ulaştık

---
+++ FoodyTekmerBusinessLayer/Concrete/ProductMenager.cs
@@ -0,0 +1,69 @@
+﻿using FoodyTekmerBusinessLayer.Abstract;
+using FoodyTekmerDataAccessLayer.Abstract;
+using FoodyTekmerEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodyTekmerBusinessLayer.Concrete
+{
+    public class ProductMenager : IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductMenager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void TAdd(Product entity)
+        {
+            if(entity.Name != "" && entity.Name.Length <=3 && entity.NewPrice > 0)
+            {
+                _productDal.Add(entity);
+            }
+            else
+            {
+                //hata mesajı
+            }            
+        }
+
+        public void TDelete(int id)
+        {
+            if(id != 0)
+            {
+                _productDal.Delete(id);
+            }
+            else
+            {
+               //hata mesajı
+            }
+           
+        }
+
+        public List<Product> TGetAllList()
+        {
+            return _productDal.GetAllList();
+        }
+
+        public Product TGetById(int id)
+        {
+           return _productDal.GetById(id);
+        }
+
+        public void TUpdate(Product entity)
+        {
+            if (entity.ProductId != 0 && entity.Name != "" && entity.Name.Length <= 3 && entity.NewPrice > 0)
+            {
+                _productDal.Update(entity);
+            }
+            else 
+            {
+                //hata mesajı
+            }
+
+        }
+    }
+}
--- FoodyTekmerEntityLayer/Concrete/Product.cs
+++ FoodyTekmerEntityLayer/Concrete/Product.cs
@@ -9,8 +9,8 @@ namespace FoodyTekmerEntityLayer.Concrete
     public class Product
     {
         public int ProductId { get; set; }
-        public int Name { get; set; }
-        public int ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
         public decimal OldPrice { get; set; }
         public decimal NewPrice { get; set; }
         public bool Status { get; set; }
