8c7ffdcdf49f550ebceef6f0653cc11afa198f34
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-02-18 16:24:08 +0300

    Anasayfa girişi prgogram css de düzenlendi

--- FoodyTekmerWebUI/Program.cs
+++ FoodyTekmerWebUI/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=default}/{action=Index}/{id?}");
 
 app.Run();
