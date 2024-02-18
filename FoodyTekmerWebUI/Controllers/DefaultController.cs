66d655d864dbee38418b78bc013d7ef58465f2bf
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2024-02-18 08:49:30 +0300

    Default controller eklendi ve içine patrialhead eklendi böylelikle temayı parçalamaya başlıyoruz

---
+++ FoodyTekmerWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PartialHead()
+        {
+            return View();
+        }
+    }
+}
