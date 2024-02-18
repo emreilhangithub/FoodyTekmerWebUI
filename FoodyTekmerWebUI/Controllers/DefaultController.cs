9cd8774392dba3e6077934b76c3abe0fd5ec5117
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-02-18 10:53:05 +0300

    2 tip yaptık 1)partial 2)compannt şeklinde

--- FoodyTekmerWebUI/Controllers/DefaultController.cs
+++ FoodyTekmerWebUI/Controllers/DefaultController.cs
@@ -6,12 +6,25 @@ namespace FoodyTekmerWebUI.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.title1 = "Ürünler";
+            ViewBag.title2 = "Ana Sayfa";
+            ViewBag.title3 = "Sayfalar";
+            ViewBag.title4 = "Ürün Listesi";
             return View();
         }
 
         public IActionResult PartialHead()
         {
-            return View();
+            return PartialView();
+        }
+
+        public IActionResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public IActionResult PartialPageHeader()
+        {           
+            return PartialView();
         }
     }
 }
